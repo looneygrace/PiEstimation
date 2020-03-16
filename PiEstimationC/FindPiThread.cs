ed04daee63800eae2df3571837a21969c1bf56e5
Grace Looney <gralloon@ut.utm.edu>
2020-03-16 16:21:27 -0500

    FindPiThread Class is written but not tested

--- PiEstimationC/FindPiThread.cs
+++ PiEstimationC/FindPiThread.cs
@@ -11,9 +11,25 @@ namespace PiEstimationC
         int numDarts;
         int count;
         Random r;
+        FindPiThread(int num)
+        {
+            numDarts = num;
+            count = 0;
+            r = new Random();
+        }
+        public int getCount()
+        {
+            return count;
+        }
         public void throwDarts()
         {
-
+            for (int i = 0; i < numDarts; i++)
+            {
+                double x = r.NextDouble();
+                double y = r.NextDouble();
+                if (Math.Sqrt(x * x + y * y) <= 1)
+                    { count = count + 1; }
+            }
         }
     }
 }
